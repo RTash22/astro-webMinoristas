a2140d59ae104234d5ac166c74e74f7623afc6d9
hazielgmz <hazielgomez033@gmail.com>
2026-03-16 03:03:49 -0600

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import vercel from '@astrojs/vercel';
+
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+
+	adapter: vercel({
+    
+  }),
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
